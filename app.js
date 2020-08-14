929cc0c518720c1aeb72cf072af0057adaa9a587
MinhaLee94 <67772844+shjune10@users.noreply.github.com>
2020-08-14 17:19:22 +0900

    JS: stroke, fill, toggle_btn

--- app.js
+++ app.js
@@ -1,33 +1,76 @@
-const canvas = document.getElementById("jsCanvas");
+const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
+const colors = document.querySelector('#colors');
+const range = document.getElementById('jsRange');
+const mode = document.getElementById('jsMode');
 
-ctx.strokeStyle = "#2C2C2C";
+const INITIAL_COLOR = "#2C2C2C";
+const CANVAS_SIZE = 700;
+
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
+
 function stopPainting() {
     painting = false;
+}
+
+function startPainting() {
+    painting = true;
 }
 
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
-    console.log(x, y);
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
-
-function onMouseDown(event) {
-    painting = true;
-}
-
-function onMouseUp(event) {
-    stopPainting();
-}
-
-function
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", () => {
+        if(filling){
+            ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+        }
+    });
 }
+
+if(colors){
+    colors.addEventListener("click", () => {
+        // canvas color should be replaced with the color clicked
+        const target = event.target.dataset.color;
+        ctx.strokeStyle = target;
+        ctx.fillStyle = target;
+    }); 
+}
+
+if(range){
+    range.addEventListener("input", () => {
+        const size = event.target.value;
+        ctx.lineWidth = size;
+    });
+}
+
+mode.addEventListener("click", () => {
+    if(filling === true){
+        filling = false;
+        mode.innerText = 'Fill';
+    } else {
+        filling = true;
+        mode.innerText = 'Paint';
+    }
+});
