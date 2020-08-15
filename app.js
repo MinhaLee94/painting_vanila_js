4bc1d78f3fad936b5cb13bbcbbf680a579ec5ac3
MinhaLee94 <67772844+shjune10@users.noreply.github.com>
2020-08-15 14:04:13 +0900

    JS: saveBtn

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext('2d');
 const colors = document.querySelector('#colors');
 const range = document.getElementById('jsRange');
 const mode = document.getElementById('jsMode');
+const saveBtn = document.getElementById('jsSave');
 
 const INITIAL_COLOR = "#2C2C2C";
 const CANVAS_SIZE = 700;
@@ -10,6 +11,8 @@ const CANVAS_SIZE = 700;
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
+ctx.fillStyle = white;
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
@@ -47,6 +50,9 @@ if(canvas){
             ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
         }
     });
+    canvas.addEventListener("contextmeu", () => {
+        event.preventDefault();
+    });
 }
 
 if(colors){
@@ -74,3 +80,13 @@ mode.addEventListener("click", () => {
         mode.innerText = 'Paint';
     }
 });
+
+if(saveBtn){
+    saveBtn.addEventListener("click", () => {
+        const image = canvas.toDataURL();
+        const link = document.createElement("a");
+        link.href = image;
+        link.download = "paint_export";
+        link.click();
+    });
+}
